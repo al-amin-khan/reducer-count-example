cf521876de95e605b5f6e509dfd324bbeb16596e
al-amin-khan <alaminkhan2010@gmail.com>
2021-01-25 20:21:25 +0600

    react reducer example

---
+++ src/components/ReducerCount/ReducerCount.js
@@ -0,0 +1,30 @@
+import React, { useReducer } from 'react';
+
+const initialState =  {
+  count: 0
+}
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return {count: state.count + 1}
+    case 'DECREMENT':
+      return {count: state.count - 1}
+    default:
+      return state
+  }
+}
+
+const ReducerCount = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <h2>React Reducer Example Using React "useReducer()" Hook.</h2>
+      <h3>Count: {state.count}</h3>
+      <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
+      <button onClick={() => dispatch({type: 'DECREMENT'})}>Increment</button>
+    </div>
+  );
+};
+
+export default ReducerCount;
